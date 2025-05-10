7428e4a458d3141973e34af5329179071129fe5e
EC2 Default User <ec2-user@ip-172-31-8-170.ap-northeast-1.compute.internal>
2025-05-10 06:28:02 +0000

    [finish]check14

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,20 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+
+private
 end
