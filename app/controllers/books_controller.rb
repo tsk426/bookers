1bcd7855d0eaf5b6ba489d22ef4fcb84c262ee05
EC2 Default User <ec2-user@ip-172-31-8-170.ap-northeast-1.compute.internal>
2025-05-12 09:40:08 +0000

    [finish]update1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,6 +18,8 @@ class BooksController < ApplicationController
     if @book.update(book_params)
     flash[:notice] = 'Book was successfully updated.'
     redirect_to book_path(@book.id)
+    else
+    render :edit
     end
   end
 
